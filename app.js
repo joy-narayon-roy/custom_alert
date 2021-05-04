0348033ee0845bd846aca182b1788d387bcb5c8f
Joy Narayon Roy <68392629+joyroy-bd@users.noreply.github.com>
2021-05-04 16:23:55 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function customAlert() {
+    let alrt = document.getElementById('alert');
+    alrt.style.display = "block";
+}
+
+function dismissAlert() {
+    let alrt = document.getElementById('alert');
+    alrt.style.display = "none";
+}
